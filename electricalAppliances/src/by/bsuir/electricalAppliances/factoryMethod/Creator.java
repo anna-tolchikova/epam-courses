d77d9d72295bac1312b6f4b8eb639f3ad6355ea0
anna-tolchikova <any.tolchikova@gmail.com>
2014-06-26 02:19:43 +0300

    del

--- electricalAppliances/src/by/bsuir/electricalAppliances/factoryMethod/Creator.java
+++
@@ -1,9 +0,0 @@
-package by.bsuir.electricalappliances.factorymethod;
-
-import by.bsuir.electricalappliances.modelAbstractions.ElectricalAppliance;
-
-public abstract class Creator {
-
-    public abstract ElectricalAppliance factoryMethod();
-
-}
